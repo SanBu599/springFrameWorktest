0c3f535fad17554ee15c6bd4a6aaa7dae1bc05a1
SanBu <qhrms369@gmail.com>
2023-07-11 23:08:03 +0900

    0711

---
+++ test1/src/main/java/ssg/com/a/dto/StudentDto.java
@@ -0,0 +1,57 @@
+package ssg.com.a.dto;
+
+import java.io.Serializable;
+
+public class StudentDto  implements  Serializable{
+	
+	private String name;
+	private int age;
+	
+	
+	
+	public StudentDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public StudentDto(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "StudentDto [name=" + name + ", age=" + age + "]";
+	}
+	
+	
+}
